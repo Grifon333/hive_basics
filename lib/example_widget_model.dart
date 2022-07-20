903a81e525e5084979a576070b46e914918dfea9
KorGan <krogen1247@gmail.com>
2022-07-20 21:14:21 +0300

    transfer Hive.initFlutter()

--- lib/example_widget_model.dart
+++ lib/example_widget_model.dart
@@ -2,9 +2,6 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class ExampleWidgetModel {
-  ExampleWidgetModel() {
-    Hive.initFlutter();
-  }
 
   void doSome() async {
     // var box = await Hive.openBox('database');
